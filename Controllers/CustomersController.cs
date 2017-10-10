e10f45ff68613f830e63a4ba391783f0a077b30d
Adam Szponar <adszponar@gmail.com>
2017-10-10 19:14:28 +0200

    Dodanie DbContext i EF

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -11,27 +11,39 @@ namespace Vidly.Controllers
     public class CustomersController : Controller
     {
         // GET: Customers
-        CustomerViewModel model = new CustomerViewModel
+        private ApplicationDbContext _context;
+
+        public CustomersController()
         {
-            Customers = new List<Customer>
-            {
-                new Customer { Id = 0, Name = "Stefan Batory" },
-                new Customer {Id = 1, Name = "Krzysztof Knoblaucht" }
-            }
-        };
+         _context = new ApplicationDbContext();   
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        //CustomerViewModel model = new CustomerViewModel
+        //{
+        //    Customers = new List<Customer>
+        //    {
+        //        new Customer { Id = 0, Name = "Stefan Batory" },
+        //        new Customer {Id = 1, Name = "Krzysztof Knoblaucht" }
+        //    }
+        //};
 
         public ActionResult CustomersIndex()
         {
 
-
-            return View(model);
+            var customers = _context.Customers.ToList();
+            return View(customers);
         }
 
         [Route("customers/details/{id}")]
         public ActionResult GetCustomer(int id)
         {
 
-            Customer cust = model.Customers.Find(c => c.Id == id);
+            Customer cust = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (cust == null)
             {
                 return HttpNotFound();

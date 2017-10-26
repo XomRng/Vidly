aeb0f47b371451e26ba2f38c063787a68a947ba0
Adam Szponar <adszponar@gmail.com>
2017-10-26 06:40:53 +0200

    HTTPS turn on
    properties of project, SSL Enabled.
    in project configuration you need to copy new  SSL url
    add global filter filters.Add(new RequireHttpsAttribute());

--- App_Start/Startup.Auth.cs
+++ App_Start/Startup.Auth.cs
@@ -54,9 +54,9 @@ namespace Vidly
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(
+               appId: "1498366846913814",
+               appSecret: "");
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{

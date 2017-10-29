f0e17ba83fbae7a1d6286a21a412cfa1388ddde0
Adam Szponar <adszponar@gmail.com>
2017-10-29 17:02:24 +0100

    Dodanie GLIMPSE oraz prezentacja cacheingu
    GLIMPSE to swietny programik pozwalajacy na badanie wydajnosci strony. must have.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Vidly.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
-        [AllowAnonymous]
+        [OutputCache(Duration = 50, Location =  OutputCacheLocation.Server, VaryByParam = "*")] // takie cos sprawia ze dany view jest przechowywane w cashe u klienta
+        //i nastepnym razem gdy odwiedzi strone, nie bedzie ona wywolywala zapytan sql itp do serwera
+        //tylko wczyta sie z pamieci klienta
         public ActionResult Index()
         {
             return View();
         }
-
+        [OutputCache(Duration = 0, VaryByParam = "*", NoStore = true)] // disabling caching
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";

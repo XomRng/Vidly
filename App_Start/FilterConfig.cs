734d5a3e49814f4f40199ef1a3ec8087b7b37864
Adam Szponar <adszponar@gmail.com>
2017-11-01 17:58:42 +0100

    Custom Error handling
    Są aż 3 typy błędów które trzeba oprogramować osobno.
    błędy wywołane przez exceptiony
    błędy z powodu odpalenia akcji/kontrolera której nie ma
    błędy w momencie wczytywania statycznych danych (np obrazek.gif) których nie ma
    
    każde trzeba oprogramować osobno

--- App_Start/FilterConfig.cs
+++ App_Start/FilterConfig.cs
@@ -9,7 +9,8 @@ namespace Vidly
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleErrorAttribute()); // to dziala na wszystkie controlery i akcje, ten filtr jest odpalany przed i po akcji, jesli jest exception to go lapie i wyswietla error view z shared
+            //ten filtr dziala tylko wtedy kiedy wystąpi exception, nie dziala na błędy 404, gdy chcemy odpalic cos co zwraca wynik akcji = 404
             filters.Add(new AuthorizeAttribute());
             filters.Add(new RequireHttpsAttribute());
 

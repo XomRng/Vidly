193ca49ff33654112d6976471ff99430d33c273d
Adam Szponar <adszponar@gmail.com>
2017-11-01 11:23:52 +0100

    Filtrowanie w okienku wyszukiwania dodane

--- Controllers/Api/CustomersController.cs
+++ Controllers/Api/CustomersController.cs
@@ -20,11 +20,15 @@ namespace Vidly.Controllers.Api
             _context = new ApplicationDbContext();
         }
         // GET /api/customers
-        public IHttpActionResult GetCustomers()
+        public IHttpActionResult GetCustomers(string query = null) // optional parameter
         {
-            var customerDtos = _context.Customers
-                .Include(c => c.MembershipType)
-                .ToList()
+            var customersQuery = _context.Customers
+                .Include(c => c.MembershipType);
+            
+            if(!String.IsNullOrWhiteSpace(query))
+                customersQuery = customersQuery.Where(c => c.Name.Contains(query));
+
+            var customerDtos = customersQuery
                 .Select(Mapper.Map<Customer, CustomerDto>);
             return Ok(customerDtos); //_context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
         }
